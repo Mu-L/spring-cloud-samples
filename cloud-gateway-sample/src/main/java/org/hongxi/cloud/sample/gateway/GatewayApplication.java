13771235ec849147ab1cb093718fd4ce56d9e0f4
javahongxi <javahongxi@qq.com>
2026-06-03 00:28:14 +0800

    去掉冗余注解

--- cloud-gateway-sample/src/main/java/org/hongxi/cloud/sample/gateway/GatewayApplication.java
+++ cloud-gateway-sample/src/main/java/org/hongxi/cloud/sample/gateway/GatewayApplication.java
@@ -3,18 +3,12 @@ package org.hongxi.cloud.sample.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 
 /**
  * Created by shenhongxi on 2020/7/12.
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-@LoadBalancerClients({
-        @LoadBalancerClient("consumer-reactive-sample"),
-        @LoadBalancerClient("consumer-sample")
-})
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
