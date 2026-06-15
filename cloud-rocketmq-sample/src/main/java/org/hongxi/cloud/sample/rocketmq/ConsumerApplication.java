bb553ab7e87e25d7c876311628a36fc378dc37aa
javahongxi <javahongxi@qq.com>
2026-06-15 09:27:08 +0800

    comments

--- cloud-rocketmq-sample/src/main/java/org/hongxi/cloud/sample/rocketmq/ConsumerApplication.java
+++ cloud-rocketmq-sample/src/main/java/org/hongxi/cloud/sample/rocketmq/ConsumerApplication.java
@@ -3,6 +3,9 @@ package org.hongxi.cloud.sample.rocketmq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Producer: consumer-dubbo-sample
+ */
 @SpringBootApplication
 public class ConsumerApplication {
     public static void main(String[] args) {
