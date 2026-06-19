4467d49d3fcd402a202a702837f010541dd27c64
javahongxi <javahongxi@qq.com>
2026-06-19 09:45:37 +0800

    remove useless import

--- cloud-consumer-sample/src/main/java/org/hongxi/cloud/sample/consumer/ConsumerApplication.java
+++ cloud-consumer-sample/src/main/java/org/hongxi/cloud/sample/consumer/ConsumerApplication.java
@@ -1,16 +1,9 @@
 package org.hongxi.cloud.sample.consumer;
 
-import feign.RequestInterceptor;
-import io.micrometer.tracing.Span;
-import io.micrometer.tracing.Tracer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by javahongxi on 2026/6/1.
