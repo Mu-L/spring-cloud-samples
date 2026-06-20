b387e4635e523a7c9a950d6b80c44b0e3a30bef7
javahongxi <javahongxi@qq.com>
2026-06-20 11:09:24 +0800

    remove redundancy @EnableDiscoveryClient

--- cloud-consumer-sample/src/main/java/org/hongxi/cloud/sample/consumer/ConsumerApplication.java
+++ cloud-consumer-sample/src/main/java/org/hongxi/cloud/sample/consumer/ConsumerApplication.java
@@ -2,13 +2,11 @@ package org.hongxi.cloud.sample.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Created by javahongxi on 2026/6/1.
  */
-@EnableDiscoveryClient
 @SpringBootApplication(exclude = {
     org.apache.dubbo.spring.boot.autoconfigure.observability.otel.OpenTelemetryAutoConfiguration.class
 })
